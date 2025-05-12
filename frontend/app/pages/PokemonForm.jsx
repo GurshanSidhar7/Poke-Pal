ba7fba66f727655bb5f9a742eba2e8efc5d9142a
Gurshan Sidhar <gurshansidhar7@gmail.com>
2025-05-11 20:18:32 -0400

    Updated api routing

--- frontend/app/pages/PokemonForm.jsx
+++ frontend/app/pages/PokemonForm.jsx
@@ -40,7 +40,7 @@ export default function PokemonForm() {
     setError(null)
 
     try {
-      const response = await fetch("https://poke-pal-backend.onrender.com", {
+      const response = await fetch("https://poke-pal-backend.onrender.com/get-pokemon", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
